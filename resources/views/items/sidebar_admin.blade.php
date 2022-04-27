35fd206c967fcb90b9418924e979f9a4b0d9ecf9
Muhammad As'adul Azmi <asadulazmi@gmail.com>
2022-04-27 20:58:11 +0700

    penambahan index

--- resources/views/items/sidebar_admin.blade.php
+++ resources/views/items/sidebar_admin.blade.php
@@ -97,7 +97,7 @@
 				</li><li>
 					<a href='?module=JS Arrays'><i class='fa fa-angle-double-right'></i>JS Arrays[ GALIH ADI PRATAMA]</a>
 				</li><li>
-					<a href='?module=JS Array Methods'><i class='fa fa-angle-double-right'>	   </i>JS Array Methods[ 		]</a>
+					<a href="{{ route('jsazmi') }}"><i class='fa fa-angle-double-right'></i>JS MUHAMMAD AS'ADUL AZMI[MUHAMMAD AS'ADUL AZMI ]</a>
 				</li><li>
 					<a href='?module=JS Array Sort'><i class='fa fa-angle-double-right'>		  </i>JS Array Sort[	]</a>
 				</li><li>
